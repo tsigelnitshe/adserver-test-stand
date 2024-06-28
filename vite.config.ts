c6058374c92a3e34d142607b0b0ccb1f9131297f
Alexey Tsigelnitskiy <tsigel@gmail.com>
2024-06-28 13:57:22 +0300

    remove dist

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dist', // укажите путь, где будет размещено приложение
   plugins: [react()],
 });
