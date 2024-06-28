6833e44235d8d8d29c85cc7616b91cf0e0a78d53
Alexey Tsigelnitskiy <tsigel@gmail.com>
2024-06-28 13:43:50 +0300

    dist

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/adserver-test-stand/dist', // укажите путь, где будет размещено приложение
+  base: '/dist', // укажите путь, где будет размещено приложение
   plugins: [react()],
 });
